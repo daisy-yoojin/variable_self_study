a2a9c8caf3a1ffa597eb34f1267603464032ab3e
daisy-yoojin <daisy.park95@gmail.com>
2020-09-16 16:27:51 +0900

    Update | NodeJS | node study

--- JS/NodeTest/main.js
+++ JS/NodeTest/main.js
@@ -1,27 +1,68 @@
-// function sum(a,b){
-//     return a+b;
-// }
-
-// console.log('sum = '+ sum(1,2));
-
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
  
 var app = http.createServer(function(request,response){
     var _url = request.url;
-    console.log(_url);
     var queryData = url.parse(_url, true).query;
-    console.log(queryData.id);
-    if(_url == '/'){
-      _url = '/index.html';
+    var pathname = url.parse(_url, true).pathname;
+    if(pathname === '/'){
+      if(queryData.id === undefined){
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+          var title = 'Welcome';
+          var description = 'Hello, Node.js';
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+        });
+      } else {
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+          var title = queryData.id;
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+        });
+      }
+    } else {
+      response.writeHead(404);
+      response.end('Not found');
     }
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
-    }
-    response.writeHead(200);
-    response.end(queryData.id);
- 
 });
 app.listen(3000);
 
@@ -34,4 +75,4 @@ http :// opentutorials.org : 3000/ main ? id=HTML&page=12
 
 // http://localhost/?id=HTML
 
-App 파일을 이용해 본문구현
+// App 파일을 이용해 본문구현
---
+++ JS/NodeTest/syntax/comparison.js
@@ -0,0 +1,6 @@
+console.log(1==1); // true
+console.log(1==2); // false
+console.log(1>2); // false
+console.log(1<2); // true
+console.log(1===1); // true - 정확성..? 더 높아서...?
+console.log(1===2); // false
