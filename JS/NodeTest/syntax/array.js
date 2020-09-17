d1a325cb64a814beece12ce4a5a25e155d2ebb77
daisy-yoojin <daisy.park95@gmail.com>
2020-09-17 12:32:43 +0900

    Update | NodeJS | node study

---
+++ JS/NodeTest/syntax/array.js
@@ -0,0 +1,9 @@
+var arr =['A','B','C','D'];
+console.log(arr[1]);
+arr[2] = 3;
+console.log(arr);
+console.log(arr.length);
+arr.push('E');
+console.log('----------------');
+console.log(arr);
+console.log(arr.length);
