74e82c92b84074dc7db45564d6df0cc519ed0a40
daisy-yoojin <daisy.park95@gmail.com>
2020-09-18 17:12:48 +0900

    Update | Angular | angular2 study

---
+++ JS/hello-angular/front-end/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root', // TAG NAME
+  templateUrl: './app.component.html', // HTML 
+  styleUrls: ['./app.component.scss'] // CSS
+})
+export class AppComponent {
+  title = 'front-end';
+}
---
+++ JS/hello-angular/front-end/src/app/hello/hello.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector:'hello',
+    // template: '<div></div>'
+    templateUrl: './hello.component.html',
+    styleUrls: ['./hello.component.css'],
+})
+export class HelloComponent{
+
+}
