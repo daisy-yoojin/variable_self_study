8e429feb2281585107d000eadd5787bbc94f38ad
daisy-yoojin <daisy.park95@gmail.com>
2021-01-12 15:58:26 +0900

    Update | React | update code for lifeCycle example

--- JS/ReactProject/hello-react/src/Counter.js
+++ JS/ReactProject/hello-react/src/Counter.js
@@ -1,9 +1,46 @@
 import React, {Component} from 'react';
 
+const Problematic= ()=>{
+    throw (new Error('버그!!!!!!!'));
+    return(
+        <div>
+
+        </div>
+    );
+};
+
 class Counter extends Component{
     state={
-        number:0
+        number:0,
+        error: false,
     }
+    constructor(props){
+        super(props);
+        console.log('constructor');
+    }
+    componentWillMount(){
+        console.log('componentWillMount (deprecated)');
+    }
+
+    componentDidMount(){
+        console.log('componentDidMount');
+    }
+
+    shouldComponentUpdate(nextProps, nextState){
+        //5의 배수라면 리렌더링하지않음
+        console.log('shouldComponentUpdate');
+        if(nextState.number % 5 == 0) return false;
+        return true;
+    }
+
+    componentWillUpdate(nextProps, nextState){
+        console.log('componentWillUpdate');
+    }
+
+    componentDidUpdate(nextProps, nextState){
+        console.log('componentDidUpdate');
+    }
+
 
     handleIncrease= ()=>{
         const {number} = this.state;
@@ -19,12 +56,21 @@ class Counter extends Component{
             })
         );
     }
+    
+    componentDidCatch(error,info){
+        this.setState({
+            error:true
+        });
+    }
 
     render(){
+        if (this.state.error) return (<h1>에러발생!</h1>);
+        console.log('render');
         return(
             <div>
                 <h1>카운터</h1>
                 <div>값: {this.state.number}</div>
+                { this.state.number === 4 && <Problematic /> }
                 <button onClick={this.handleIncrease}>+</button>
                 <button onClick={this.handleDecrease}>-</button>
             </div>
