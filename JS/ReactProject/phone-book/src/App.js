796aab8cbf35356f50a8a2081b937b2a74f7c1de
daisy-yoojin <daisy.park95@gmail.com>
2021-01-13 17:25:22 +0900

    Update | React | React Array construting and rendering & about map

--- JS/ReactProject/phone-book/src/App.js
+++ JS/ReactProject/phone-book/src/App.js
@@ -1,20 +1,41 @@
 import React, {Component} from 'react'
-import logo from './logo.svg';
 import './App.css';
 import PhoneForm from './components/PhoneForm';
 
 class App extends Component{
+  id =2
+  state={
+    information:[
+      {
+        id:0,
+        name:'김가나',
+        phone:'010-0000-0000'
+      },
+      {
+        id:1,
+        name:'김다라',
+        phone:'010-0000-0001'
+      }
+    ]
+  } 
+
   handleCreate = (data)=>{
     console.log(data);
+    const {information} = this.state;
+    this.setState({
+      information: information.concat({ id: this.id++, ...data })
+    })
   };
 
   render() {
-      return(
-        <div>
-          <PhoneForm
-            onCreate={this.handleCreate}/>
-        </div>
-      );
+    const {information} = this.state;
+    return(
+      <div>
+        <PhoneForm
+          onCreate={this.handleCreate}/>
+          {JSON.stringify(information)}
+      </div>
+    );
   }
 }
 
--- JS/ReactProject/phone-book/src/components/PhoneForm.js
+++ JS/ReactProject/phone-book/src/components/PhoneForm.js
@@ -28,8 +28,7 @@ class PhoneForm extends Component{
                 <input placeholder="이름" value={this.state.name} onChange={this.handleChange} name="name"/>
                 <input placeholder="전화번호" value={this.state.phone} onChange={this.handleChange} name="phone"/>
                 <button type="submit">등록</button>
-            </form>
-
+            </form> 
         );
     }
 }
