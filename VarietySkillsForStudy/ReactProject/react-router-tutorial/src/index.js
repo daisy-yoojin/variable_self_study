9910713291933c548afa75a74a6a9e042223b760
daisy-yoojin <daisy.park95@gmail.com>
2021-02-08 14:15:57 +0900

    Update | React | react router example app

--- VarietySkillsForStudy/ReactProject/react-router-tutorial/src/index.js
+++ VarietySkillsForStudy/ReactProject/react-router-tutorial/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './client/Root';
-import registerServiceWorker from './registerServiceWorker';
+// import * as serviceWorker from './serviceWorker';
 import './index.css';
 
 ReactDOM.render(<Root />, document.getElementById('root'));
-registerServiceWorker();
+// serviceWorker.unregister();
--- VarietySkillsForStudy/ReactProject/react-router-tutorial/src/shared/App.js
+++ VarietySkillsForStudy/ReactProject/react-router-tutorial/src/shared/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Menu from '../components/Menu';
+import { Home, About, Posts } from '../pages';
 
 class App extends Component {
     render() {
         return (
             <div>
-                Hello React-Router
+                <Menu/>
+                <Route exact path="/" component={Home}/>                
+                {/* Route 배치 순서 중요 */}
+                <Switch>
+                    <Route path="/about/:name" component={About}/>
+                    <Route path="/about" component={About}/>
+                </Switch>
+                <Route path="/posts" component={Posts}/>
             </div>
         );
     }
 }
-
 export default App;
