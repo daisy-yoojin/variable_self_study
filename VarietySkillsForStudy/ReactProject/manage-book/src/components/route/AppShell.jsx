c7e8b990269fdc13a7df991c8946a12dab05f550
daisy-yoojin <daisy.park95@gmail.com>
2021-02-10 08:45:45 +0900

    Update | React | Add Loading

---
+++ VarietySkillsForStudy/ReactProject/manage-book/src/components/route/AppShell.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+const styles={
+    root:{
+        flexGrow: 1,
+    },
+    menuButton:{
+        marginRight: 'auto'
+    }
+}
+
+class AppShell extends Component{
+
+}
+
+export default withStyle(styles)(AppShell);
