eb8d2dbe7e755f02803d423b7d7c2f5f61f180e4
daisy-yoojin <daisy.park95@gmail.com>
2021-02-10 13:33:23 +0900

    Update | React | add Left navbar

--- VarietySkillsForStudy/ReactProject/manage-book/src/components/route/NavBar.jsx
+++ VarietySkillsForStudy/ReactProject/manage-book/src/components/route/NavBar.jsx
@@ -1,60 +1,68 @@
-import React,{Component} from 'react';
-import SignIn from './../login/SignIn'
+import React, { Component } from "react";
+import SignIn from "./../login/SignIn";
 //Material UI
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
 //import MenuIcon from '@material-ui/icons/Menu';
-import LeftNavBar from './LeftNavBar';
+import LeftNavBar from "./LeftNavBar";
 
-class NavBar extends Component{
-    constructor(props){
-        super(props);
-        this.state={
-            isModalOpen:false,
-            isLeftNavOpen:false,
-        };
-    }
-    openLeftNav = ()=>{
-        this.setState({isLeftNavOpen: true});
-    }
-    clsoeLeftNav = ()=>{
-        this.setState({isLeftNavOpen: false});
-    }
+class NavBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isModalOpen: false,
+      isLeftNavOpen: false,
+    };
+  }
+  openLeftNav = () => {
+    this.setState({ isLeftNavOpen: true });
+  };
+  clsoeLeftNav = () => {
+    this.setState({ isLeftNavOpen: false });
+  };
 
-    openModal = ()=>{
-        this.setState({isModalOpen: true});
-    }
+  openModal = () => {
+    this.setState({ isModalOpen: true });
+  };
 
-    closeModal=()=>{
-        this.setState({isModalOpen: false});
-    }
+  closeModal = () => {
+    this.setState({ isModalOpen: false });
+  };
 
-    render(){
-        return (
-            <div>
-                <AppBar position="static">
-                    <Toolbar>
-                        <IconButton edge="start" color="inherit" aria-label="Menu" onClick={this.openLeftNav}>                            
-                            <LeftNavBar isOpen={this.state.isLeftNavOpen} isClose={this.clsoeLeftNav}/>
-                        </IconButton>
-                        
-                        <Typography variant="h6" style={style}>
-                            Account Application
-                        </Typography>
-                        <Button color='inherit' onClick={this.openModal}>Login</Button>
-                        <SignIn isOpen={this.state.isModalOpen} isClose={this.closeModal}/>
-                    </Toolbar>
-                </AppBar>
-            </div>
-        );
-        
-    }
+  render() {
+    return (
+      <div>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="Menu"
+              onClick={this.openLeftNav}
+            >
+              <LeftNavBar
+                isOpen={this.state.isLeftNavOpen}
+                isClose={this.clsoeLeftNav}
+              />
+            </IconButton>
+            <Typography variant="h6" style={style}>
+              App
+            </Typography>
+            <Button color="inherit" onClick={this.openModal}>
+              Login
+            </Button>
+            <SignIn isOpen={this.state.isModalOpen} isClose={this.closeModal} />
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
 }
 
-const style ={
-    flexGrow: 1
-}
+const style = {
+  flexGrow: 1,
+};
 export default NavBar;
