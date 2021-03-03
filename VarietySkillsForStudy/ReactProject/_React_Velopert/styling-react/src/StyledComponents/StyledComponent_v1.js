f2e12453988ed55c5b26980df74db032048c4b31
daisy-yoojin <daisy.park95@gmail.com>
2021-03-03 14:31:18 +0900

    Commit | React | About Sass Styling, CSS module

--- VarietySkillsForStudy/ReactProject/_React_Velopert/styling-react/src/StyledComponents/StyledComponent_v1.js
+++ VarietySkillsForStudy/ReactProject/_React_Velopert/styling-react/src/StyledComponents/StyledComponent_v1.js
@@ -1,40 +1,11 @@
 import React from "react";
 import styled, { css } from "styled-components";
 
-const sizes = {
-  desktop: 1024,
-  tablet: 768
-};
-//위에 있는 size 객체에 따라 자동으로 media쿼리 함수를 만들어 준다.
-// 참고 : https://www.styled-components.com/docs/advanced#media-templates
-const media = Object.keys(sizes).reduce((acc, label) => {
-  acc[label] = (...args) => css`
-    @media(max-width: ${sizes[label]/16}em){
-      ${css(...args)};
-    }
-  `;
-  return acc;
-}, {});
-
 /*props로 넣어준 값을 직접 전달해 줄 수 있다.*/
 const Box = styled.div`
   background: ${(props) => props.color || "blue"};
   padding: 1res;
   display: flex;
-  /* 기본적으로 가로 크기 1024px에 가운데 정렬 하고
-  가로 크기가 작아짐에 따라 크기를 줄이고
-  768px미만이 되면 꽉 채운다. */
-  width: 1024px;
-  margin: 0 auto;
-  /* @media (max-width: 1024px){
-    width: 768px;
-  }
-  @media (max-width: 768px){
-    width: 100%;
-  } */
-  ${media.desktop`width: 768px`};
-  ${media.tablet`width: 1024px`};
-  
 `;
 
 const Button = styled.button`
