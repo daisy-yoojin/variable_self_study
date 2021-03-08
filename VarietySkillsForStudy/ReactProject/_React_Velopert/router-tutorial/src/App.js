51b57be0f6e7d7ec91bcc9b54fcaa5bb699e8bc3
daisy-yoojin <daisy.park95@gmail.com>
2021-03-08 11:12:10 +0900

    Commit | React | router with router and use history

--- VarietySkillsForStudy/ReactProject/_React_Velopert/router-tutorial/src/App.js
+++ VarietySkillsForStudy/ReactProject/_React_Velopert/router-tutorial/src/App.js
@@ -2,7 +2,8 @@ import "./App.css";
 import { Link, Route } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
-import Profile from "./Profile";
+import Profiles from "./Profiles";
+import HistorySample from "./HistorySample";
 
 function App() {
   return (
@@ -15,16 +16,17 @@ function App() {
           <Link to="/about">소개</Link>
         </li>
         <li>
-          <Link to="/profile/velopert">velopert 프로필</Link>
+          <Link to="/profiles">프로필</Link>
         </li>
         <li>
-          <Link to="/profile/gildong">gildong 프로필</Link>
+          <Link to="/history">History예제</Link>
         </li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact={true} />
       <Route path={["/about", "/info"]} component={About} />
-      <Route path="/profile/:username" component={Profile} />
+      <Route path="/profiles" component={Profiles} />
+      <Route path="/history" component={HistorySample} />
     </div>
   );
 }
